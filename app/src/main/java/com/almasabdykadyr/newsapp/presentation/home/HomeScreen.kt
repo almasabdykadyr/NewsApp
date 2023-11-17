b77c4101153e8a1ece2c96442d280d639362c56b
Almas Abdykadyr <almasabdykadir@gmail.com>
2023-11-17 20:43:33 +0600

    add ArticlesList, EmptyScreen, HomeScreen

---
+++ app/src/main/java/com/almasabdykadyr/newsapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,55 @@
+package com.almasabdykadyr.newsapp.presentation.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import com.almasabdykadyr.newsapp.R
+import com.almasabdykadyr.newsapp.domain.model.Article
+import com.almasabdykadyr.newsapp.presentation.Dimens.MediumPadding1
+
+@Composable
+fun HomeScreen(articles: LazyPagingItems<Article>, navigation: (String) -> Unit) {
+
+    val titles by remember {
+        derivedStateOf {
+            if (articles.itemCount > 10) {
+                articles.itemSnapshotList.items.slice(0..9)
+            } else {
+                ""
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = MediumPadding1)
+            .statusBarsPadding()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .width(150.dp)
+                .height(30.dp)
+                .padding(horizontal = MediumPadding1)
+        )
+
+        Spacer(modifier = Modifier.height(MediumPadding1))
+
+
+    }
+}
