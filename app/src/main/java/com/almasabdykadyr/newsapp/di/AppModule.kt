785c058f012b2042ea660076426f966eefc27040
Almas Abdykadyr <almasabdykadir@gmail.com>
2023-11-28 01:13:08 +0600

    update constans

--- app/src/main/java/com/almasabdykadyr/newsapp/di/AppModule.kt
+++ app/src/main/java/com/almasabdykadyr/newsapp/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.almasabdykadyr.newsapp.di
 
 import android.app.Application
+import androidx.room.Room
+import com.almasabdykadyr.newsapp.data.local.NewsDao
+import com.almasabdykadyr.newsapp.data.local.NewsDatabase
+import com.almasabdykadyr.newsapp.data.local.NewsTypeConverter
 import com.almasabdykadyr.newsapp.data.manager.LocalUserManagerImpl
 import com.almasabdykadyr.newsapp.data.remote.NewsApi
 import com.almasabdykadyr.newsapp.data.repository.NewsRepositoryImpl
@@ -11,7 +15,9 @@ import com.almasabdykadyr.newsapp.domain.usecases.appentry.ReadAppEntry
 import com.almasabdykadyr.newsapp.domain.usecases.appentry.SaveAppEntry
 import com.almasabdykadyr.newsapp.domain.usecases.news.GetNewsUseCase
 import com.almasabdykadyr.newsapp.domain.usecases.news.NewsUseCases
+import com.almasabdykadyr.newsapp.domain.usecases.news.SearchNewsUseCase
 import com.almasabdykadyr.newsapp.util.Constants.BASE_URL
+import com.almasabdykadyr.newsapp.util.Constants.NEWS_DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,18 +39,14 @@ object AppModule {
     @Provides
     @Singleton
     fun provideAppEntryUseCases(localUserManager: LocalUserManager) = AppEntryUseCases(
-        readAppEntry = ReadAppEntry(localUserManager),
-        saveAppEntry = SaveAppEntry(localUserManager)
+        readAppEntry = ReadAppEntry(localUserManager), saveAppEntry = SaveAppEntry(localUserManager)
     )
 
     @Provides
     @Singleton
     fun provideNewsApi(): NewsApi {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(NewsApi::class.java)
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build().create(NewsApi::class.java)
     }
 
     @Provides
@@ -54,6 +56,21 @@ object AppModule {
     @Provides
     @Singleton
     fun provideNewsUseCases(newsRepository: NewsRepository) = NewsUseCases(
-        getNewsUseCase = GetNewsUseCase(newsRepository)
+        getNewsUseCase = GetNewsUseCase(newsRepository),
+        searchNewsUseCase = SearchNewsUseCase(newsRepository)
     )
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        application: Application
+    ): NewsDatabase = Room.databaseBuilder(
+        context = application.applicationContext,
+        klass = NewsDatabase::class.java,
+        name = NEWS_DATABASE_NAME
+    ).addTypeConverter(NewsTypeConverter()).fallbackToDestructiveMigration().build()
+
+    @Provides
+    @Singleton
+    fun provideNewsDao(newsDatabase: NewsDatabase): NewsDao = newsDatabase.newsDao
 }
--- app/src/main/java/com/almasabdykadyr/newsapp/util/Constants.kt
+++ app/src/main/java/com/almasabdykadyr/newsapp/util/Constants.kt
@@ -9,4 +9,6 @@ object Constants {
     const val API_KEY = "304423a13bf94bae893d0a4390cd7654"
 
     const val BASE_URL = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME = "news_db"
 }
