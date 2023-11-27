341a967cae236f2a73367b315dae7d86f5b8a570
Almas Abdykadyr <almasabdykadir@gmail.com>
2023-11-28 01:03:53 +0600

    add SearchNewsUseCase to NewsUseCases

--- app/src/main/java/com/almasabdykadyr/newsapp/domain/usecases/news/NewsUseCases.kt
+++ app/src/main/java/com/almasabdykadyr/newsapp/domain/usecases/news/NewsUseCases.kt
@@ -1,5 +1,6 @@
 package com.almasabdykadyr.newsapp.domain.usecases.news
 
 data class NewsUseCases(
-    val getNewsUseCase: GetNewsUseCase
+    val getNewsUseCase: GetNewsUseCase,
+    val searchNewsUseCase: SearchNewsUseCase
 )
