2954a700ae72893b5d511335ffa8b62ce3f5a0a6
Almas Abdykadyr <almasabdykadir@gmail.com>
2023-11-30 00:45:38 +0600

    update GetArticlesUseCase

--- app/src/main/java/com/almasabdykadyr/newsapp/domain/usecases/news/GetArticlesUseCase.kt
+++ app/src/main/java/com/almasabdykadyr/newsapp/domain/usecases/news/GetArticlesUseCase.kt
@@ -1,12 +1,14 @@
 package com.almasabdykadyr.newsapp.domain.usecases.news
 
 import com.almasabdykadyr.newsapp.data.local.NewsDao
+import com.almasabdykadyr.newsapp.domain.model.Article
+import kotlinx.coroutines.flow.Flow
 
 class GetArticlesUseCase(
     private val newsDao: NewsDao
 ) {
 
-    operator fun invoke() {
-        newsDao.getArticles()
+    operator fun invoke(): Flow<List<Article>> {
+        return newsDao.getArticles()
     }
 }
