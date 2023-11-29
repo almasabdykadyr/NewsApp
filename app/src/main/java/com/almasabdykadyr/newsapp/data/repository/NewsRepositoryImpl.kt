2013048ef3bcfa3b38a99f1ff9a96436cce7cbf6
Almas Abdykadyr <almasabdykadir@gmail.com>
2023-11-29 20:47:18 +0600

    update repository

--- app/src/main/java/com/almasabdykadyr/newsapp/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/almasabdykadyr/newsapp/data/repository/NewsRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.almasabdykadyr.newsapp.data.remote.NewsApi
 import com.almasabdykadyr.newsapp.data.remote.NewsPagingSource
+import com.almasabdykadyr.newsapp.data.remote.SearchNewsPagingSource
 import com.almasabdykadyr.newsapp.domain.model.Article
 import com.almasabdykadyr.newsapp.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
@@ -19,6 +20,20 @@ class NewsRepositoryImpl(private val newsApi: NewsApi): NewsRepository {
                 NewsPagingSource(
                     newsApi = newsApi,
                     sources = sources.joinToString(separator = ",")
+                )
+            }
+        ).flow
+    }
+
+    override fun searchNews(searchQuery: String, sources: List<String>): Flow<PagingData<Article>> {
+
+        return Pager(
+            config = PagingConfig(pageSize = 10),
+            pagingSourceFactory = {
+                SearchNewsPagingSource(
+                    api = newsApi,
+                    sources = sources.joinToString(separator = ","),
+                    searchQuery = searchQuery
                 )
             }
         ).flow
